98086bfecdd8dcc5a6213f8b3d25bfbd9625a484
iggor_kurr <kurinnoyi@gmail.com>
2015-07-28 12:41:34 +0300

    fix google maps for test env

--- index.js
+++ index.js
@@ -6,16 +6,22 @@ module.exports = {
     app.import('vendor/map.css');
   },
   contentFor: function(type, config) {
-    if (type === 'body-footer') {
-      var libs = '<script src="http://api-maps.yandex.ru/2.0/?load=package.standard&lang=ru-Ru"></script>';
-
+    if (type === 'body-footer' && config.environment !== 'test') {
+      var libs = '<script src="https://api-maps.yandex.ru/2.0/?load=package.standard&lang=ru-Ru"></script>';
       if (config['ember-cli-map'] && config['ember-cli-map'].googleApiKey) {
         var key = config['ember-cli-map'].googleApiKey;
-        libs = libs + '<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&key=' + key + '&libraries=places"></script>';
+        libs = libs + '<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false&key=' + key + '&libraries=places"></script>';
+      } else {
+        libs = libs + '<script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false&libraries=places"></script>';
       }
-      else {
-        libs = libs + '<script src="https://maps.googleapis.com/maps/api/js?v=3.exp&sensor=false&libraries=places"></script>';
-      }
+
+      return libs;
+    }
+
+    if (type === 'test-body-footer' && config.environment === 'test') {
+      var libs = '<script src="http://api-maps.yandex.ru/2.0/?load=package.standard&lang=ru-Ru"></script>';
+
+      libs = libs + '<script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false&libraries=places"></script>';
 
       return libs;
     }
